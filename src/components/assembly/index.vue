0c6fe2a661546b0950efac4a83ffc92e69ddd3ea
xiaolin <462441355@qq.com>
2017-12-16 20:02:14 +0800

    * 页面

--- src/components/assembly/index.vue
+++ src/components/assembly/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    介绍
+  <div class="components-container">
+    <code>
+      这里暂时列出了自己在项目中用到的组件和一些自己封装的组件，因为很多组件会和业务后高度的耦合，而且第三方封装的组件灵活性可控性都不高
+    </code>
   </div>
 </template>
