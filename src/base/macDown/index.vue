f47d52c7d056ee7e7de34dcb61919c887789f006
xiaolin <462441355@qq.com>
2017-12-17 11:47:47 +0800

    * macDown组件

--- src/base/macDown/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    sssss
-  </div>
-</template>
--- src/components/assembly/markdown.vue
+++ src/components/assembly/markdown.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="components-container">
-    <mac-down></mac-down>
+    <code>
+      Markdown 我们这里选用了 <a href="https://github.com/sparksuite/simplemde-markdown-editor" target="_blank">simplemde-markdown-editor</a>
+    </code>
+     <div class="editor-container">
+       <mar-editor id='contentEditor' ref="contentEditor" v-model='content' :height="300" :zIndex='20'></mar-editor>
+     </div>
+    <el-button @click='markdown2Html' style="margin-top:80px;" type="primary">转为HTML<i class="el-icon-document el-icon--right"></i></el-button>
+    <div v-html="html"></div>
   </div>
 </template>
 
 
 <script>
-import macDown from "@/base/macDown";
-  export default {
-  components: { macDown },
+import marEditor from "@/base/MarDowndEditor";
+export default {
+  components: { marEditor },
   data() {
     return {
-      content: "Tinymce"
+      content: "## Simplemde",
+      html: ""
     };
+  },
+  methods: {
+    markdown2Html() {}
   }
 };
 </script>
