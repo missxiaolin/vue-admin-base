d61ba25a993ce4493c78692f18fc8deb9426c985
xiaolin <462441355@qq.com>
2017-12-21 10:17:15 +0800

    * 新增json组件

---
+++ src/base/jsonEditor/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='json-editor'>
+    <textarea ref='textarea'></textarea>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'jsonEditor',
+  }
+</script>
--- src/components/assembly/jsonEditor.vue
+++ src/components/assembly/jsonEditor.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="components-container" style='height:100vh'>
-    json
+    <code>jsonEditor is base on  <a href="https://github.com/codemirror/CodeMirror" target="_blank">CodeMirrorr</a> , lint base on json-lint </code>
+    <div class="editor-container">
+      <json-editor ref="jsonEditor" v-model="value"></json-editor>
+    </div>
   </div>
 </template>
 
 <script>
+import jsonEditor from 'base/jsonEditor'
 const jsonData = '[{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"UKOIL"}]'
 
 export default {
-  components: {},
+  components: {jsonEditor},
   data() {
     return {
       value: JSON.parse(jsonData)
