941370295ca58334fd266f632e2ff584341f44da
xiaolin <462441355@qq.com>
2017-12-24 11:37:37 +0800

    * 上传图片组件

--- src/components/assembly/avatarUpload.vue
+++ src/components/assembly/avatarUpload.vue
@@ -10,11 +10,19 @@
     <el-button
       type="primary"
       icon="upload"
-      style="position: absolute;bottom: 15px;margin-left: 40px;">
+      style="position: absolute;bottom: 15px;margin-left: 40px;"
+      @click="imagecropperShow=true">
       修改头像
     </el-button>
 
-    <image-cropper></image-cropper>
+    <image-cropper
+      :width="300"
+      :height="300"
+      url="https://httpbin.org/post"
+      @close='close'
+      @crop-upload-success="cropSuccess"
+      :key="imagecropperKey"
+      v-show="imagecropperShow"></image-cropper>
   </div>
 </template>
 
@@ -26,14 +34,19 @@ export default {
   components: { ImageCropper, PanThumb },
   data() {
     return {
-      image: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191'
-    }
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      image: "https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191"
+    };
   },
   methods: {
     cropSuccess(resData) {
-      this.imagecropperShow = false
-      this.imagecropperKey = this.imagecropperKey + 1
-      this.image = resData.files.avatar
+      this.imagecropperShow = false;
+      this.imagecropperKey = this.imagecropperKey + 1;
+      this.image = resData.files.avatar;
+    },
+    close() {
+      this.imagecropperShow = false;
     }
   }
 };
