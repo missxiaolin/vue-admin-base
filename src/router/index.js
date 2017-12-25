da0cc9339cd66cfcc6e73d381278a11603695d85
xiaolin <462441355@qq.com>
2017-12-25 14:52:48 +0800

    * 测试 路由更改

--- src/router/index.js
+++ src/router/index.js
@@ -7,95 +7,63 @@ Vue.use(Router)
 /* layout */
 import layout from '@/components/layout/Layout'
 
-export const asyncRouterMap = [{
-  path: '*',
-  redirect: '/404',
-  hidden: true
-}, {
-  path: '/error',
-  name: '401',
-  component: layout,
-  redirect: '/error/401',
-  hidden: true,
-  children: [{
-    path: '401',
-    component: _import('errorPage/401')
-  }]
-}, {
-  path: '/404',
-  name: '404',
-  component: _import('errorPage/404'),
-  hidden: true
-}, {
-  path: '/login', // 登录
-  name: 'login',
-  hidden: true,
-  component: _import('login/login')
-}, {
-  path: '/',
-  component: layout,
-  redirect: '/dashboard',
-  hidden: true,
-  children: [{
-    path: 'dashboard',
-    component: _import('dashboard/index')
-  }]
-}, {
-  path: '/introduction',
-  component: layout,
-  redirect: '/introduction/index',
-  icon: 'icon-ren',
-  noDropdown: true,
-  children: [{
-    path: 'index',
-    component: _import('introduction/index'),
-    name: '简述'
-  }]
-}, {
-  path: '/excel',
-  component: layout,
-  redirect: '/excel/download',
-  name: 'excel',
-  icon: 'icon-Excel',
-  noDropdown: false,
-  children: [{
-    path: 'download',
-    component: _import('excel/index'),
-    name: 'excel导出'
-  }]
-}, {
-  path: '/assembly',
-  component: layout,
-  redirect: '/assembly/index',
-  name: '组件',
-  icon: 'icon-zujianku',
-  noDropdown: false,
-  children: [{
-    path: 'index',
-    component: _import('assembly/index'),
-    name: '介绍'
+export const asyncRouterMap = [
+  { path: '*', redirect: '/404', hidden: true },
+  {
+    path: '/error',
+    name: '401',
+    component: layout,
+    redirect: '/error/401',
+    hidden: true,
+    children: [
+      { path: '401', component: _import('errorPage/401') }
+    ]
+  },
+  { path: '/404', name: '404', component: _import('errorPage/404'), hidden: true },
+  // 登录
+  { path: '/login', name: 'login', hidden: true, component: _import('login/login') },
+  {
+    path: '/',
+    component: layout,
+    redirect: '/dashboard',
+    hidden: true,
+    children: [
+      { path: 'dashboard', component: _import('dashboard/index') }
+    ]
   }, {
-    path: 'tinymce',
-    component: _import('assembly/tinymce'),
-    name: '富文本编辑器'
+    path: '/introduction',
+    component: layout,
+    redirect: '/introduction/index',
+    icon: 'icon-ren',
+    noDropdown: true,
+    children: [
+      { path: 'index', component: _import('introduction/index'), name: '简述' }]
   }, {
-    path: 'markdown',
-    component: _import('assembly/markdown'),
-    name: 'Markdown'
+    path: '/excel',
+    component: layout,
+    redirect: '/excel/download',
+    name: 'excel',
+    icon: 'icon-Excel',
+    noDropdown: false,
+    children: [
+      { path: 'download', component: _import('excel/index'), name: 'excel导出' }
+    ]
   }, {
-    path: 'jsoneditor',
-    component: _import('assembly/jsonEditor'),
-    name: 'jsoneditor'
-  }, {
-    path: 'avatarupload',
-    component: _import('assembly/avatarUpload'),
-    name: '头像上传'
-  }, {
-    path: 'dropzone',
-    component: _import('assembly/dropzone'),
-    name: 'Dropzone'
+    path: '/assembly',
+    component: layout,
+    redirect: '/assembly/index',
+    name: '组件',
+    icon: 'icon-zujianku',
+    noDropdown: false,
+    children: [
+      { path: 'index', component: _import('assembly/index'), name: '介绍' },
+      { path: 'tinymce', component: _import('assembly/tinymce'), name: '富文本编辑器' },
+      { path: 'markdown', component: _import('assembly/markdown'), name: 'Markdown' },
+      { path: 'jsoneditor', component: _import('assembly/jsonEditor'), name: 'jsoneditor' },
+      { path: 'avatarupload', component: _import('assembly/avatarUpload'), name: '头像上传' },
+      { path: 'dropzone', component: _import('assembly/dropzone'), name: 'Dropzone' },
+      { path: 'sticky', component: _import('assembly/sticky'), name: 'Sticky' }]
   }]
-}]
 
 export default new Router({
   mode: 'history',
