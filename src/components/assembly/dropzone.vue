a68e0f31f7e6fee7e2c98187a9ee796f2c461506
xiaolin <462441355@qq.com>
2017-12-25 10:20:27 +0800

    * 初始化组件

--- src/components/assembly/dropzone.vue
+++ src/components/assembly/dropzone.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    图片上传
+  <div class="components-container">
+    <code>
+      基于<a class='link-type' href='https://github.com/rowanwins/vue-dropzone'> dropzone </a>
+    </code>
+    <div class="editor-container">
+      <dropzone  id="myVueDropzone" url="https://httpbin.org/post"></dropzone>
+    </div>
   </div>
 </template>
+
+<script>
+import Dropzone from 'base/Dropzone'
+
+export default {
+  components: { Dropzone },
+  methods: {
+    dropzoneS(file) {
+      console.log(file)
+      this.$message({ message: '上传成功', type: 'success' })
+    },
+    dropzoneR(file) {
+      console.log(file)
+      this.$message({ message: '删除成功', type: 'success' })
+    }
+  }
+}
+</script>
