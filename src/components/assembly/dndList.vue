73866a3bf3128f78603aa9d7f1049339e1095e0c
xiaolin <462441355@qq.com>
2017-12-26 12:33:57 +0800

    * 拖拽

--- src/components/assembly/dndList.vue
+++ src/components/assembly/dndList.vue
@@ -16,6 +16,29 @@
 import DndList from "base/twoDndList";
 
 export default {
-  components: { DndList }
-}
+  components: { DndList },
+  data() {
+    return {
+      list1: [],
+      list2: [
+        {id: 1,author: "小林",title: 'title1'},
+        {id: 2,author: "小林",title: 'title2'},
+        {id: 3,author: "小林",title: 'title3'},
+        {id: 4,author: "小林",title: 'title4'},
+        {id: 5,author: "小林",title: 'title5'},
+        {id: 6,author: "小林",title: 'title6'},
+        {id: 7,author: "小林",title: 'title7'},
+        {id: 8,author: "小林",title: 'title8'},
+        {id: 9,author: "小林",title: 'title9'},
+        {id: 10,author: "小林",title: 'title10'},
+        {id: 11,author: "小林",title: 'title11'},
+        {id: 12,author: "小林",title: 'title12'},
+      ]
+    };
+  },
+  created() {
+    this.list1 = this.list2.splice(0, 5);
+  },
+  methods: {}
+};
 </script>
