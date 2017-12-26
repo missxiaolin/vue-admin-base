0c12ce3e37dbab5fa09df22f36ed40d514e27494
xiaolin <462441355@qq.com>
2017-12-26 18:22:36 +0800

    * 添加折线图组件

---
+++ src/base/Charts/lineMarker.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      折线图组件
+  </div>
+</template>
--- src/components/charts/line.vue
+++ src/components/charts/line.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-      折线图
+  <div class="components-container" style='height:100vh'>
+    <div class='chart-container'>
+      <line-marker height='100%' width='100%'></line-marker>
+    </div>
   </div>
 </template>
+
+<script>
+import lineMarker from 'base/Charts/lineMarker'
+
+export default {
+  components: { lineMarker }
+}
+</script>
+
+<style scoped>
+.chart-container{
+  position: relative;
+  width: 100%;
+  height: 80%;
+}
+</style>
