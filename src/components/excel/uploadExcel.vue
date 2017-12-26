e4a050bc565207737b226639a5cb6ebe168b7c47
xiaolin <462441355@qq.com>
2017-12-26 14:20:51 +0800

    * excel

--- src/components/excel/uploadExcel.vue
+++ src/components/excel/uploadExcel.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="app-container">
-      上传excel
-      <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
-      <el-table-column v-for='item of tableHeader' :prop="item" :label="item" :key='item'>
-      </el-table-column>
+    <upload-excel @on-selected-file='selected'></upload-excel>
+    <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
+        <el-table-column v-for='item of tableHeader' :prop="item" :label="item" :key='item'>
+        </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
-import uploadExcel from 'base/UploadExcel/'
+import uploadExcel from "base/UploadExcel";
 
 export default {
   components: { uploadExcel },
@@ -17,13 +17,13 @@ export default {
     return {
       tableData: [],
       tableHeader: []
-    }
+    };
   },
   methods: {
     selected(data) {
-      this.tableData = data.results
-      this.tableHeader = data.header
+      this.tableData = data.results;
+      this.tableHeader = data.header;
     }
   }
-}
+};
 </script>
