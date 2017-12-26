e122853ddbc54d5b7c3db68514b360d52061ab62
xiaolin <462441355@qq.com>
2017-12-26 11:58:51 +0800

    * 增加组件测试

--- src/components/assembly/dndList.vue
+++ src/components/assembly/dndList.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-      列表拖拽
+  <div class="components-container">
+    <code>
+        drag-list base on 
+        <a href="https://github.com/SortableJS/Vue.Draggable" target="_blank">
+        Vue.Draggable
+        </a>
+    </code>
+    <div class="editor-container">
+       <dnd-list :list1="list1" :list2="list2" list1Title="头条列表" list2Title="文章池"></dnd-list>
+    </div>
   </div>
 </template>
+
+<script>
+import DndList from "base/twoDndList";
+
+export default {
+  components: { DndList }
+}
+</script>
